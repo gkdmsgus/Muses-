2d7d638b779f55d2612a7641bb2e5b68d8a782ad
Leeseungchul <chulee503@naver.com>
2026-01-09 18:08:52 +0900

    [feat] Navbar & Footer 구현 완료 (#9)
    * feat: Footer 구현 완료
    
    * feat: Header 구현 중
    
    * style: 헤더 muses 홈 링크로 변경
    
    * style: 로고 폰트 변경
    
    * style: 전체 폰트 수정
    
    * style: 전체 font 변경 완료
    
    * style: 로그인/회원가입 버튼색 수정
    
    * chore: EventListPage 라우트 설정
    
    * style: font 스타일, 버튼 hover시 scale로 변경
    
    * style: border white80 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import ProjectListPage from './pages/ProjectListPage';
 import LoginSignupPage from './pages/LoginSignupPage';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+import EventListPage from './pages/EventListPage';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/projects" element={<ProjectListPage />} />
+        <Route path="/events" element={<EventListPage />} />
         <Route path="/login" element={<LoginSignupPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
