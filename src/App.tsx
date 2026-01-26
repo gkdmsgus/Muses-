a227a92e2a33dedf115d84fe540a9fc282e4e833
hyunbin <guieuna3@gmail.com>
2026-01-27 00:09:24 +0900

    [feat] 마이페이지 구현 (#8)
    * feat: 마이페이지 구현-1
    
    * fix: 임시커밋
    
    * merge 문제 해결
    
    * feat: 프로필 수정페이지, 크리에이터 신청 폼 추가 + 폰트, 색상 추가
    
    * docs: 헤더 주석 추가
    
    * fix: 폰트 수정, 코드레빗 코멘트 수정
    
    * fix: 오타수정
    
    * style: 들여쓰기 및 색상 명 수정
    
    * fix: shadow 수정
    
    * fix: 가독성, 코멘트 수용
    
    ---------
    
    Co-authored-by: sooh329 <glay@kakao.com>
    Co-authored-by: Kim Soohyun <134276531+sooh329@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import LandingPage from './pages/LandingPage';
 import ProjectListPage from './pages/ProjectListPage';
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignupPage';
-import OnBoardingPage from './pages/OnBoardingPage';
+import MyPage from './pages/MyPage';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import EventListPage from './pages/EventListPage';
 import ProjectDetailPage from './pages/ProjectDetailPage';
-import EventDetailPage from './pages/EventDetailPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import OnBoardingPage from './pages/OnBoardingPage';
 
 function App() {
   return (
@@ -20,11 +20,11 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/projects" element={<ProjectListPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/onboarding" element={<OnBoardingPage />} />
         <Route path="/events" element={<EventListPage />} />
         <Route path="/project/:id" element={<ProjectDetailPage />} />
-        <Route path="/events/:id" element={<EventDetailPage />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
