6e3725f14d9bbd7684c1d0283848c36b0a88803f
Leeseungchul <chulee503@naver.com>
2026-01-30 22:38:44 +0900

    [feat] CreateProject페이지 UI 구현 (#31)
    * refactor: 푸터 높이 제한
    
    * feat: 네비게이션, 1단계 개요 완료
    
    * refactor: prettier ( ' -> " ) 변경
    
    * fix: 해당 단계를 넘어간 후 Check표시가 뜨도록 수정
    
    * test: FundingData 타입 관리 임시 추가
    
    * feat: 재사용하는 입력 필드, 폼 컴포넌트 분리
    
    * feat: 2단계 펀딩 완료
    
    * refactor: 분리한 필드, 폼 적용
    
    * style: prettier ( ' -> " ) 변경
    
    * refactor: 네비게이션 로고 png 통합
    
    * refactor: 스텝퍼 가로줄 픽셀조정 & 폼필드 text 색상 변경 & 2단계 펀딩 수정사항 반영
    
    * feat: 4단계 스토리 완료
    
    * feat: 5단계 정보 완료
    
    * feat: 3단계 리워드 완료
    
    * chore: /create 라우트 설정
    
    * feat: CreateProjectPage 네비게이션 다음 단계 버튼 활성화
    
    * refactor: 코드 래빗 리뷰 반영

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import EventListPage from './pages/EventListPage';
 import ProjectDetailPage from './pages/ProjectDetailPage';
+import CreateProjectPage from './pages/CreateProjectPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import OnBoardingPage from './pages/OnBoardingPage';
@@ -16,7 +17,6 @@ function App() {
     <BrowserRouter>
       <ScrollToTop />
       <AppLayout />
-      <Footer />
     </BrowserRouter>
   );
 }
@@ -36,7 +36,7 @@ function ScrollToTop() {
 function AppLayout() {
   const { pathname } = useLocation();
   //const hideHeaderPrefixes = ['/create-project', '/admin']; // 헤더를 숨길 경로 설정 예시, 하단에 추가하시면 됩니다.
-  const hideHeaderPrefixes = ['/admin'];
+  const hideHeaderPrefixes = ['/admin', '/create'];
   const shouldHideHeader = hideHeaderPrefixes.some((prefix) =>
     pathname.startsWith(prefix)
   );
@@ -53,6 +53,7 @@ function AppLayout() {
         <Route path="/events" element={<EventListPage />} />
         <Route path="/project/:id" element={<ProjectDetailPage />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/create" element={<CreateProjectPage />} />
       </Routes>
       <Footer />
     </>
